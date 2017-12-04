3b8234345cc10af9476d20624ef493c5d2757b2d
marcoadenadai <marcoadenadai@users.noreply.github.com>
2017-12-04 08:14:34 -0200

    x
    x

--- T2.asm
+++ T2.asm
@@ -2,81 +2,60 @@ TITLE PACMAN
 .MODEL SMALL
 .STACK 100h
 .DATA
-    Palette db 256*4 dup (0)
+;filename db 'menu00.bmp',0
+filename db 64 dup (0)
+filehandle dw ?
+Header db 54 dup (0)
+Palette db 256*4 dup (0)
+ScrLine db 320 dup (0)
+ErrorMsg db 'Error', 13, 10,'$'
+
+menu00 db 'menu00.bmp',0
+menu01 db 'menu01.bmp',0
+menu1_00 db 'menu1_00.bmp',0
+menu1_02 db 'menu1_02.bmp',0
+menu1_10 db 'menu1_10.bmp',0
+menu1_12 db 'menu1_12.bmp',0
 
 ;------------------------------------------------------------------------------------------------------------------
 .CODE
-;linhaSharp
-;sublinha
-;Tsharp
-w equ 10
-h equ 5
+PROC copyName
+        push    BX
+        push    CX
+        xor     BX,BX
+        mov     CX,064d
+zera:   
+        mov     filename[BX],00h
+        loop    zera
+        
+
+
+        pop     BX
+        pop     CX
+copyName ENDP
+
 PROC MAIN
         mov     AX,@DATA
         mov     DS,AX
 
-;setRes_VGA640x480:
-;        mov     AX,04F02h
-;        mov     BX,101h
-;        int     010h
+        mov     AH,00h
+        mov     AL,013h
+        int     010h
 
-        mov     ah, 0
-        mov     al, 13h
-        int     10h
+        mov     
+        mov     filename, menu00
 
-
-; draw upper line
-        mov     CX, 100+w  ; column
-        mov     DX, 20     ; row
-        mov     AL, 0Dh    ; white
-u1:     mov     AH, 0ch    ; put pixel
-        int     10h
-
-        dec     CX
-        cmp     CX, 100
-        jae     u1
-
-; draw bottom line:
-        mov     CX, 100+w  ; column
-        mov     DX, 20+h   ; row
-        mov     AL, 01h     ; white
-u2:     mov     AH, 0ch    ; put pixel
-        int     10h
-
-        dec     CX
-        cmp     CX, 100
-        ja      u2
-
-; draw left line:
-        mov     CX, 100    ; column
-        mov     DX, 20+h   ; row
-        mov     AL, 30h     ; white
-u3:     mov     AH, 0ch    ; put pixel
-        int     10h
-
-        dec     DX
-        cmp     DX, 20
-        ja      u3
-
-
-; draw right line:
-        mov     CX, 100+w  ; column
-        mov     DX, 20+h   ; row
-        mov     AL, 02ch     ; white
-u4:     mov     AH, 0ch    ; put pixel
-        int     10h
-
-        dec     DX
-        cmp     DX, 20
-        ja      u4
-
-
-; pause the screen for dos compatibility:
-;wait for keypress
-        mov     AH,00
-        int     16h
+        call    OpenFile
+        call    ReadHeader
+        call    ReadPalette
+        call    CopyPal
+        call    CopyBitmap
 
 end_main:
+        mov     AH,01h
+        int     21h
+        int     21h
+
         mov     AH,00       ; return to text mode:
         mov     AL,03       ;text mode 3
         int     10h
@@ -118,126 +97,136 @@ Teclado ENDP
 ;;==============================================================================
 ;;==============================================================================
 ;;==============================================================================
-PROC OpenFile
-        mov     AH, 03Dh
-        xor     AL, AL
-        mov     DX, offset filename
-        int     021h
+proc OpenFile
+    mov ah, 3Dh
+    xor al, al
+    mov dx, offset filename
+    int 21h
 
-        jc      openerror
-        mov     [filehandle], ax
-        ret
+    jc openerror
+    mov [filehandle], ax
+    ret
 
-        openerror:
-        mov     DX, offset ErrorMsg
-        mov     AH, 09h
-        int     021h
-        ret
-ENDP OpenFile
+    openerror:
+    mov dx, offset ErrorMsg
+    mov ah, 9h
+    int 21h
+    ret
+endp OpenFile
 ;;==============================================================================
-PROC ReadHeader    ; Read BMP file header, 54 bytes
-        mov     AH,03fh
-        mov     BX, [filehandle]
-        mov     CX,54d
-        mov     DX,offset Header
-        int     021h
-        ret
-ENDP    ReadHeader
+proc ReadHeader
+    mov ah,3fh
+    mov bx, [filehandle]
+    mov cx,54
+    mov dx,offset Header
+    int 21h
+    ret
+    endp ReadHeader
+    proc ReadPalette
+
+    ; Read BMP file color palette, 256 colors * 4 bytes (400h)
+    mov ah,3fh
+    mov cx,400h
+    mov dx,offset Palette
+    int 21h
+    ret
+endp ReadPalette
 ;;==============================================================================
-PROC ReadPalette
-; Read BMP file color palette, 256 colors * 4 bytes (400h)
-        mov     AH,03fh
-        mov     CX,0400h
-        mov     DX,offset Palette
-        int     021h
-        ret
-ENDP ReadPalette
+proc CopyPal
+    ; Copy the colors palette to the video memory
+    ; The number of the first color should be sent to port 3C8h
+    ; The palette is sent to port 3C9h
+    mov si,offset Palette
+    mov cx,256
+    mov dx,3C8h
+    mov al,0
+
+    ; Copy starting color to port 3C8h
+    out dx,al
+
+    ; Copy palette itself to port 3C9h
+    inc dx
+    PalLoop:
+
+    ; Note: Colors in a BMP file are saved as BGR values rather than RGB.
+
+    mov al,[si+2] ; Get red value.
+    shr al,1 ; Max. is 255, but video palette maximal
+    shr al,1
+    ; value is 63. Therefore dividing by 4.
+
+    out dx,al ; Send it.
+    mov al,[si+1] ; Get green value.
+    shr al,1
+    shr al,1
+    out dx,al ; Send it.
+    mov al,[si] ; Get blue value.
+    shr al,1
+    shr al,1
+    out dx,al ; Send it.
+    add si,4 ; Point to next color.
+
+    ; (There is a null chr. after every color.)
+
+    loop PalLoop
+    ret
+endp CopyPal
 ;;==============================================================================
-PROC CopyPal
+proc CopyBitmap
 
-        ; Copy the colors palette to the video memory
-        ; The number of the first color should be sent to port 3C8h
-        ; The palette is sent to port 3C9h
-
-        mov     SI,offset Palette
-        mov     CX,256d
-        mov     DX,03C8h
-        mov     AL,00h
-
-        ; Copy starting color to port 3C8h
-        out     DX,AL
-
-        ; Copy palette itself to port 3C9h
-        inc     DX
-
-        PalLoop:
-        ; Note: Colors in a BMP file are saved as BGR values rather than RGB.
-
-        mov     AL,[SI+2] ; Get red value.
-        shr     AL,2 ; Max. is 255, but video palette maximal
-                     ; value is 63. Therefore dividing by 4.
-        out     DX,AL ; Send it.
-
-        mov     AL,[SI+1] ; Get green value.
-        shr     AL,2
-        out     DX,AL ; Send it.
-
-        mov     AL,[SI] ; Get blue value.
-        shr     AL,2
-        out     DX,AL ; Send it.
-        add     SI,4 ; Point to next color.
-
-        ; (There is a null chr. after every color.)
-
-        loop PalLoop
-        ret
-ENDP CopyPal
-;;==============================================================================
-PROC CopyBitmap
     ; BMP graphics are saved upside-down.
     ; Read the graphic line by line (200 lines in VGA format),
     ; displaying the lines from bottom to top.
-        mov     AX, 0A000h
-        mov     ES, AX
-        mov     CX,200d
-        PrintBMPLoop:
-        push    CX
 
-        ; di = cx*320, point to the correct screen line
+    mov ax, 0A000h
+    mov es, ax
+    mov cx,200
+    PrintBMPLoop:
+    push cx
 
-        mov     DI,CX
-        shl     CX,6
-        shl     DI,8
-        add     DI,CX
+    ; di = cx*320, point to the correct screen line
 
-        ; Read one line
+    mov di,cx
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    shl cx,1
+    push cx
+    mov cx,08h
+    shl di,cl
+    pop cx
+    add di,cx
 
-        mov     AH,3fh
-        mov     CX,320
-        mov     DX,offset ScrLine
-        int     21h
+    ; Read one line
 
-        ; Copy one line into video memory
+    mov ah,3fh
+    mov cx,320
+    mov dx,offset ScrLine
+    int 21h
 
-        cld
+    ; Copy one line into video memory
 
-        ; Clear direction flag, for movsb
+    cld 
 
-        mov     CX,320
-        mov     SI,offset ScrLine
-        rep     movsb
+    ; Clear direction flag, for movsb
 
-        ; Copy line to the screen
-        ;rep movsb is same as the following code:
-        ;mov es:di, ds:si
-        ;inc si
-        ;inc di
-        ;dec cx
-        ;loop until cx=0
+    mov cx,320
+    mov si,offset ScrLine
+    rep movsb 
 
-        pop     CX
-        loop    PrintBMPLoop
-        ret
+    ; Copy line to the screen
+    ;rep movsb is same as the following code:
+    ;mov es:di, ds:si
+    ;inc si
+    ;inc di
+    ;dec cx
+    ;loop until cx=0
+
+    pop cx
+    loop PrintBMPLoop
+    ret
 endp CopyBitmap
 ;;==============================================================================
 END MAIN
